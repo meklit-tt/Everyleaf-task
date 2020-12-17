aa3d94fe5f847b877cbb62a806eb13e543051810
meklit <meklit_teshome@diveintocode.jp>
2020-12-17 14:25:58 +0900

    spec added

--- Gemfile
+++ Gemfile
@@ -26,6 +26,9 @@ gem 'bootsnap', '>= 1.1.0', require: false
 group :development, :test do
 
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+
+  gem 'rspec-rails', '~> 3.8'
+  gem 'factory_bot_rails'
 end
 
 group :development do
@@ -39,6 +42,9 @@ end
 group :test do
 
   gem 'capybara', '>= 2.15'
+  
+  gem 'webdrivers'
+
   gem 'selenium-webdriver'
 
   gem 'chromedriver-helper'
---
+++ spec/models/task_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+describe 'Task model function', type: :model do
+  describe 'Validation test' do
+    context 'If the task title is empty' do
+      it 'It's hard to Validation' do
+        task = Task.new(title: '', content: 'Failure test')
+        expect(task).not_to be_valid
+      end
+    end
+　　context 'If the task details are empty' do
+      it 'Validation is caught' do
+        # Write content here
+      end
+    end
+    context 'When the content is described in the task title and details' do
+      it 'Validation passes' do
+        # Write content here
+      end
+    end
+  end
