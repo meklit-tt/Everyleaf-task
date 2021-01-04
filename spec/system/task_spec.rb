841032a045b9d359973773532ee6e8f32eeb1ee8
meklit <meklit_teshome@diveintocode.jp>
2021-01-04 18:31:04 +0900

    rspec order adede

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -3,24 +3,25 @@ RSpec.describe 'Task management function', type: :system do
       describe 'New creation function' do
           context 'When creating a new task' do
                it 'Should display created task' do
-                 visit new_task_path
-                    fill_in 'Title', with: 'Task2'
-                       fill_in 'Detail', with: 'details2'
+                visit new_task_path
+                    fill_in 'Title', with: 'Task3'
+                       fill_in 'Detail', with: 'details3'
                           click_button 'Create Task'
                               expect(page).to have_content 'task created'
                                end
                                end
                               end
+
                               describe 'show creation function' do
                                   context 'When creating a new task' do
                                        it 'Should show created task' do
                                          visit new_task_path
-                                            fill_in 'Title', with: 'Task2'
-                                               fill_in 'Detail', with: 'details2'
+                                            fill_in 'Title', with: 'Task3'
+                                               fill_in 'Detail', with: 'details3'
                                                  click_button 'Create Task'
                                                   click_button 'Show'
-                                                      expect(page).to have_content 'Task2'
-                                                        expect(page).to have_content 'details2'
+                                                      expect(page).to have_content 'Task3'
+                                                        expect(page).to have_content 'details3'
                                                        end
                                                        end
                                                       end
@@ -28,12 +29,12 @@ RSpec.describe 'Task management function', type: :system do
                                                           context 'When creating a new task' do
                                                                it 'Should edit created task' do
                                                                  visit new_task_path
-                                                                    fill_in 'Title', with: 'Task2'
-                                                                       fill_in 'Detail', with: 'details2'
+                                                                    fill_in 'Title', with: 'Task3'
+                                                                       fill_in 'Detail', with: 'details3'
                                                                           click_button 'Create Task'
                                                                           click_button 'Edit'
-                                                                              expect(page).to have_content 'Task2'
-                                                                                expect(page).to have_content 'details2'
+                                                                              expect(page).to have_content 'Task3'
+                                                                                expect(page).to have_content 'details3'
                                                                                end
                                                                                end
                                                                               end
@@ -41,12 +42,18 @@ RSpec.describe 'Task management function', type: :system do
                                                                                   context 'When deleting a new task' do
                                                                                        it 'Should display deleted task' do
                                                                                             visit new_task_path
-                                                                                             fill_in 'Title', with: 'Task2'
-                                                                                             fill_in 'Detail', with: 'details2'
+                                                                                             fill_in 'Title', with: 'Task3'
+                                                                                             fill_in 'Detail', with: 'details3'
                                                                                               click_button 'Create Task'
                                                                                               click_button 'Destroy'
-                                                                                            
+
                                                                                                        end
                                                                                                        end
                                                                                                       end
+                                                                                                      context 'When tasks are arranged in descending order of creation date and time' do
+                                                                                                        it 'New task is displayed at the top' do
+                                                                                                          visit tasks_path
+                                                                                                          assert Task.all.order(created_at: "desc")
+      end
+    end
   end
