793cf103500ce79bfefea4e22987c84b614ef1d7
meklit <meklit_teshome@diveintocode.jp>
2021-01-22 00:31:27 +0900

    error handling aded

--- spec/system/label_spec.rb
+++ spec/system/label_spec.rb
@@ -2,24 +2,24 @@ require 'rails_helper'
 RSpec.describe 'Label function', type: :system do
   before do
     FactoryBot.create(:user, name: 'thisme',
-                             email: 'thits@me.com',
-                             password: 'password',
-                             password_confirmation: 'password')
+                             email: 'this@me.com',
+                             password: '098765',
+                             password_confirmation: '098765')
     visit new_session_path
-    fill_in 'Email', with: 'ange@gmail.com'
-    fill_in 'Password', with: 'password'
+    fill_in 'Email', with: 'this@me.com'
+    fill_in 'Password', with: '098765'
     click_button 'Log in'
     @user = User.first
-    FactoryBot.create(:label, name: "Work", user_id: @user.id)
-    FactoryBot.create(:label, name: "School", user_id: @user.id)
-    FactoryBot.create(:label, name: "Social", user_id: @user.id)
+    FactoryBot.create(:label, name: "good", user_id: @user.id)
+    FactoryBot.create(:label, name: "poor", user_id: @user.id)
+
   end
 
   describe 'New creation function' do
     context 'When creating a new label' do
       it 'Should display created Label' do
         visit new_label_path
-        fill_in 'Name', with: 'Work'
+        fill_in 'Name', with: 'good'
         click_button 'Create Label'
         expect(page).to have_content 'successfully created.'
       end
@@ -29,16 +29,15 @@ RSpec.describe 'Label function', type: :system do
   describe 'Adding multiple labels to tasks function' do
     context 'When you add labels to task' do
       it 'The creates task with label' do
-        Label.create(name: 'Work')
-        Label.create(name: 'Social')
+        Label.create(name: 'good')
+        Label.create(name: 'poor')
         visit new_task_path
         fill_in 'Title', with: 'Task1'
-        fill_in 'Content', with: 'details'
+        fill_in 'Detail', with: 'details'
         fill_in 'Deadline', with: '002020-11-24'
-        select 'Complete'
-        select 'High'
-        select 'Work'
-        select 'Social'
+
+        select 'good'
+        select 'poor'
         click_button 'Create Task'
         expect(page).to have_content 'Task was successfully created.'
       end
@@ -48,14 +47,15 @@ RSpec.describe 'Label function', type: :system do
   describe 'Search function' do
     context 'When you search by label' do
       it "Filter by tasks that include label selected" do
-        label = Label.create(name: 'Work')
-        task = Task.create(title: "title1", content: "content1", deadline: "2021/1/1", status:"Complete", priority: "Low", user_id: @user.id)
+        label = Label.create(name: 'good')
+        task = Task.create(title: "title1", detail: "content1", deadline: "2021/1/1", status:"Complete", priority: "Low", user_id: @user.id)
         labelling = Labelling.create(label_id: label.id)
         visit tasks_path
-        select 'Work'
+        select 'good'
         click_on 'search'
-        expect(page).to have_content 'Work'
+        expect(page).to have_content 'good'
         assert Task.ransack(title:[:q])
       end
     end
   end
+end
